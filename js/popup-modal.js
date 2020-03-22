1141e85240b9857941f10d00ab14bde8e9ecd2e6
wdbyb <58622202+wdbyb@users.noreply.github.com>
2020-03-22 20:10:03 +0200

    Почти финал

---
+++ js/popup-modal.js
@@ -0,0 +1,27 @@
+var link = document.querySelector(".button-contacts");
+
+var popup = document.querySelector(".modal");
+var close = popup.querySelector(".modal-close");
+
+var login = popup.querySelector("[name=name]");
+
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+  login.focus();
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+    }
+  }
+});
