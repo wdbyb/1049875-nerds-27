127742f2f6e991d644f53aa652296ce0945b2fd9
wdbyb <58622202+wdbyb@users.noreply.github.com>
2020-03-25 17:45:29 +0200

    Итс э файнал каунтдаун

--- js/popup-modal.js
+++ js/popup-modal.js
@@ -1,27 +1,54 @@
 var link = document.querySelector(".button-contacts");
-
 var popup = document.querySelector(".modal");
 var close = popup.querySelector(".modal-close");
-
+var form = popup.querySelector("form");
 var login = popup.querySelector("[name=name]");
+var email = popup.querySelector("[name=email]");
+var textarea = popup.querySelector("[name=textarea]");
 
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
+}
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
+  popup.classList.remove("modal-hide");
   popup.classList.add("modal-show");
   login.focus();
 });
 
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
+  popup.classList.remove("modal-error");
   popup.classList.remove("modal-show");
+  popup.classList.add("modal-hide");
 });
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
     if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-error");
       popup.classList.remove("modal-show");
+      popup.classList.add("modal-hide");
+    }
+  }
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!login.value || !email.value || !textarea.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("name", name.value);
     }
   }
 });
